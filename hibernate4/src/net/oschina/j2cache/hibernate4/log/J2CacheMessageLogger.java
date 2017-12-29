e4b7f6cb3fad2022368dd4c5cfcddbc1d9904ca3
Winter Lau <ld@oschina.net>
2017-12-29 13:33:36 +0800

    源码文件增加协议说明

--- hibernate4/src/net/oschina/j2cache/hibernate4/log/J2CacheMessageLogger.java
+++ hibernate4/src/net/oschina/j2cache/hibernate4/log/J2CacheMessageLogger.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2015-2017.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.oschina.j2cache.hibernate4.log;
 
 import org.hibernate.internal.CoreMessageLogger;
@@ -6,7 +21,6 @@ import org.jboss.logging.annotations.Message;
 import org.jboss.logging.annotations.MessageLogger;
 
 import static org.jboss.logging.Logger.Level.WARN;
-
 
 @MessageLogger(projectCode = "HHH")
 public interface J2CacheMessageLogger extends CoreMessageLogger {
