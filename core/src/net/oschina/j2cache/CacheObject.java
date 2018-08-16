9075fd8037dd7203c6e90c0dbc0292376ea8841f
Winter Lau <ld@oschina.net>
2018-08-16 14:11:02 +0800

    rawValue 方法改为 protected

--- core/src/net/oschina/j2cache/CacheObject.java
+++ core/src/net/oschina/j2cache/CacheObject.java
@@ -75,7 +75,7 @@ public class CacheObject {
 	 * @return cache object include null object
 	 */
 	public Object getValue() {
-		if (value != null && (value.getClass().equals(NullObject.class)||value.getClass().equals(Object.class)))
+		if (value == null || value.getClass().equals(NullObject.class) || value.getClass().equals(Object.class))
 			return null;
 		return value;
 	}
@@ -84,7 +84,7 @@ public class CacheObject {
 	 * 返回实际缓存的对象
 	 * @return cache raw object
 	 */
-	public Object rawValue() {
+	protected Object rawValue() {
 		return value;
 	}
 
