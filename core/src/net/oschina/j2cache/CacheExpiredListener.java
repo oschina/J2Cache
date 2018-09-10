daf415b74867573608c10cc1dbf05d47fd730787
Winter Lau <ld@oschina.net>
2018-09-10 17:00:01 +0800

    增加方法注释

--- core/src/net/oschina/j2cache/CacheExpiredListener.java
+++ core/src/net/oschina/j2cache/CacheExpiredListener.java
@@ -22,6 +22,11 @@ package net.oschina.j2cache;
  */
 public interface CacheExpiredListener {
 
+	/**
+	 * 缓存因为超时失效后触发的通知
+	 * @param region 缓存 region
+	 * @param key 缓存 key
+	 */
 	void notifyElementExpired(String region, String key) ;
 
 }
